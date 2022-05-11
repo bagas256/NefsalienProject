60a769fde6a3029f59aa4569c888daf109695ea2
Bagas Wijaya <bagaswijaya256@gmail.com>
2022-05-11 19:58:34 +0700

    Authentication Register and Login.

---
+++ Areas/Identity/Data/UserAuthentication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace NefsalienProject.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the UserAuthentication class
+    public class UserAuthentication : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName ="nvarchar(100)")]
+        public int FirstName { get; set; }
+        
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public int LastName { get; set; }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NefsalienProject.Areas.Identity.Data;
+using NefsalienProject.Data;
+
+[assembly: HostingStartup(typeof(NefsalienProject.Areas.Identity.IdentityHostingStartup))]
+namespace NefsalienProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<NefsalienDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("NefsalienDbContextConnection")));
+
+                services.AddDefaultIdentity<UserAuthentication>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<NefsalienDbContext>();
+            });
+        }
+    }
+}
